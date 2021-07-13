c15679eaca96172bbef76ffa4d7eac99f18b87b6
ikura1 <ikura.lab@gmail.com>
2021-07-13 10:40:33 +0900

    feat: step5 add markers

--- starter/src/app.js
+++ starter/src/app.js
@@ -29,8 +29,8 @@ function loadMapsJSAPI() {
 }
 
 function runApp() {
-  console.log("Maps JS API loaded")
   const map = displayMap()
+  const markers = addMarkers(map)
 }
 
 function displayMap() {
@@ -42,6 +42,38 @@ function displayMap() {
   const mapDiv = document.getElementById("map")
   // create map instance
   return new google.maps.Map(mapDiv, mapOptions)
+}
+
+function addMarkers(map) {
+  const locations = {
+    operaHouse: { lat: -33.8567844, lng: 151.213108 },
+    tarongaZoo: { lat: -33.8472767, lng: 151.2188164 },
+    manlyBeach: { lat: -33.8209738, lng: 151.2563253 },
+    hyderPark: { lat: -33.8690081, lng: 151.2052393 },
+    theRocks: { lat: -33.8587568, lng: 151.2058246 },
+    circularQuay: { lat: -33.858761, lng: 151.2055688 },
+    harbourBridge: { lat: -33.852228, lng: 151.2038374 },
+    kingsCross: { lat: -33.8737375, lng: 151.222569 },
+    botanicGardens: { lat: -33.864167, lng: 151.216387 },
+    museumOfSydney: { lat: -33.8636005, lng: 151.2092542 },
+    maritimeMuseum: { lat: -33.869395, lng: 151.198648 },
+    kingStreetWharf: { lat: -33.8665445, lng: 151.1989808 },
+    aquarium: { lat: -33.869627, lng: 151.202146 },
+    darlingHarbour: { lat: -33.87488, lng: 151.1987113 },
+    barangaroo: { lat: -33.8605523, lng: 151.1972205 },
+  }
+
+  const markers = []
+  for (const location in locations) {
+    const markerOptions = {
+      map: map,
+      position: locations[location],
+      icon: "./img/custom_pin.png",
+    }
+    const marker = new google.maps.Marker(markerOptions)
+    markers.push(marker)
+  }
+  return markers
 }
 
 loadMapsJSAPI()
