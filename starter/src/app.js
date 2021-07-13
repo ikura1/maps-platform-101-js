308463fdf0e910b579290657f2181c4d44d766d3
ikura1 <ikura.lab@gmail.com>
2021-07-13 10:24:46 +0900

    feat: step4

--- starter/src/app.js
+++ starter/src/app.js
@@ -13,3 +13,35 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+function loadMapsJSAPI() {
+  const googleMapsAPIKey = "AIzaSyDxduAHWk4xW4lbJaLYpMjVG5nwYuAD6CA"
+  const googleMapsAPIURI = `https://maps.googleapis.com/maps/api/js?key=${googleMapsAPIKey}&callback=runApp`
+
+  const script = document.createElement("script")
+  script.src = googleMapsAPIURI
+  script.defer = true
+  script.async = true
+
+  window.runApp = runApp
+
+  document.head.appendChild(script)
+}
+
+function runApp() {
+  console.log("Maps JS API loaded")
+  const map = displayMap()
+}
+
+function displayMap() {
+  const mapOptions = {
+    center: { lat: -33.860664, lng: 151.208138 },
+    zoom: 14,
+  }
+  //   getElement
+  const mapDiv = document.getElementById("map")
+  // create map instance
+  return new google.maps.Map(mapDiv, mapOptions)
+}
+
+loadMapsJSAPI()
